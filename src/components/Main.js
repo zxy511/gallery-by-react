10bcdd2bee9ab2511a0448abb4689f94b8d71f00
zhangxy <2030184523@qq.com>
2018-02-07 17:43:52 +0800

    init project

---
+++ src/components/Main.js
@@ -0,0 +1,42 @@
+require('normalize.css/normalize.css');
+require('styles/main.scss');
+
+import React from 'react';
+
+// 获取图片相关函数
+var imageDatas = require('../data/imageDatas.json');
+
+// let yeomanImage = require('../images/yeoman.png');
+
+// 利用自执行函数，将图片名信息转成图片URL路径信息
+imageDatas = (function getImageURL(imageDatasArr){
+	for (var i = 0, j = imageDatasArr.length; i < j; i++){
+		var singleImageData = imageDatasArr[i];
+
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+		imageDatasArr[i] = singleImageData;
+	}
+
+	return imageDatasArr;
+})(imageDatas);
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      	<section className = "stage">
+      		<section className = "img-sec">
+      		</section>
+
+      		<nav className = "controller-nav">
+      		</nav>
+      	</section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
